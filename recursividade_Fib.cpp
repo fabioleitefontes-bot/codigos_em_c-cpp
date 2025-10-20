d87f059bf1c662108161909f493f9a55a1d3daa6
Fabio.Fontes <fabioleitefontes@gmail.com>
2025-10-20 12:34:03 -0300

    Refactor Fibonacci implementation
    Refactor Fibonacci function to initialize array and simplify recursion.

--- recursividade_Fib.cpp
+++ recursividade_Fib.cpp
@@ -1,5 +1,5 @@
 /*
-	Name: texto.3
+	Name: Recursividade_fib
 	Author: Fabio.Fonts 
 	Date: 01/09/25 08:50
 	Description: fibonaci com recursividade
@@ -7,35 +7,32 @@
  #include<windows.h>
  #include<stdio.h>
  #include<locale.h>
-void Fibonacci(long int[], int, int);
+void Fibonacci(long long[], int, int);
  main()
  
  {
     setlocale(LC_ALL,"portuguese");
-    int i =2;
+    int i;
     int fib = 0;
  	scanf("%d", &fib);
- 	  long int soma[fib];
- 	Fibonacci(soma, fib, i);
+ 	  long long soma[fib] = {0};
+ 	  	 soma[0] = 1;
+		 soma[1] = 1;
+ 	Fibonacci(soma, fib, 2);
  } //fim do progama
- void Fibonacci(long int soma[], int fib, int i ){
- 	if(fib<2){
+ void Fibonacci(long long soma[], int fib, int i ){
+ 
+	soma[i] = soma[i-2] + soma[i-1];
+	i++;
 	 
-	 soma[0] = 1;
-	 soma[1] = 1;
-	 printf("Fib[%d] = %lld", fib,soma[fib]);
-	 return;
-}
-	 else
-	 	soma[i] = soma[i-2] + soma[i-1];
-	 	
-	 i++;
-	 Fibonacci(soma, fib, i);
-	 
-	 if(fib==i+1){
-	 	printf("Fib[%d] = %lld", fib,soma[i]);
+	 if(fib<=2){
+		 printf("Fib[%d] = %lld", fib,soma[fib]);
+		 return;
+	}
+	 if(fib==i){
+	 	printf("Fib[%d] = %lld", fib,soma[i-1]);
 	 	return;
  }
- 	
+ 		 Fibonacci(soma, fib, i);
  	
  }
